c06f1c7f5b952af6d9d9cce295e13186e5a7b2c4
Youssef Diouri <youssefdiouri4@gmail.com>
2022-02-16 08:02:39 +0100

    Adding React template to project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/**
+=========================================================
+* Soft UI Dashboard React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-material-ui
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "App";
+
+// Soft UI Dashboard React Context Provider
+import { SoftUIControllerProvider } from "context";
+
+import { AuthProvider } from "auth-context/auth.context";
+
+let user = localStorage.getItem("user");
+user = JSON.parse(user);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <SoftUIControllerProvider>
+      <AuthProvider userData={user}>
+        <App />
+      </AuthProvider>
+    </SoftUIControllerProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
